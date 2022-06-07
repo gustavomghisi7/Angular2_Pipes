6c408bf150fc8550571e8876080a3982686cb8cb
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-06-07 15:10:29 -0300

    locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import '@angular/common/locales/global/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,20 @@ import { CamelCasePipe } from './camel-case.pipe';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    /*
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
+    */
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [ SettingsService ],
+      useFactory: (settingsService: any) => settingsService.getLocale()
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
